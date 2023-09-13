189ee7d682932a7ff1413e8c193afff09fd5c477
massymassy <139942115+massy-yossy@users.noreply.github.com>
2023-09-13 13:02:15 +0900

    added cors white list

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
+    "http://localhost:3000","https://nextjs-blog-todos-xi-ten.vercel.app"
 ]
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
